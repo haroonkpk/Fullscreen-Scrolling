97718bcafcb5ca571657ce9999e593a2beb4df15
haroonkpk <haroonkhanlala47@gmail.com>
2026-01-21 17:06:54 +0500

    Refactor RootLayout component to simplify locale handling and remove async logic

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,12 @@
 import "./globals.css";
 
-export default async function LocaleLayout({
+export default function RootLayout({
   children,
-  params,
 }: {
   children: React.ReactNode;
-  params: Promise<{ locale: string }>;
 }) {
-  // Ensure that the incoming `locale` is valid
-  const { locale } = await params;
-
-  // Determine text direction based on locale
-  const isRTL = ["ar"].includes(locale); // Add other RTL languages as needed
-  const direction = isRTL ? "rtl" : "ltr";
+  const locale = "en";
+  const direction = "ltr";
 
   return (
     <html lang={locale} dir={direction}>
@@ -20,7 +14,6 @@ export default async function LocaleLayout({
         <link rel="icon" href="/assets/svgs/logo.svg" type="image/svg+xml" />
       </head>
       <body className={direction}>
-        {/* This is the main content of the page */}
         <main className={direction}>{children}</main>
       </body>
     </html>
